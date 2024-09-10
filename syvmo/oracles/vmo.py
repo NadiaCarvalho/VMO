61a560fb434a00eb706c59586585bba5d9b2b7e6
Nádia Carvalho <nadiacarvalho118@gmail.com>
2024-09-10 10:41:36 +0100

    Update vmo.py

--- syvmo/oracles/vmo.py
+++ syvmo/oracles/vmo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 """
-This script defines the VMO oracle class
-based on the code in https://github.com/wangsix/vmo/blob/master/vmo/VMO/oracle.py
+This script defines the VMO (Variable Memory Oracle) class,
+based on the code in https://github.com/wangsix/vmo/blob/master/vmo/VMO/oracle.py.
 """
 
 import math
@@ -16,10 +16,24 @@ from ..cdist_fixed import fixed_cdist
 
 class VMO(FactorOracle):
     """
-    Class VMO
+    VMO (Variable Memory Oracle) is a class that extends FactorOracle.
+    It implements an oracle that creates transitions between states based on 
+    distances between symbols and adds latent information for variable-length 
+    patterns. It is mainly used for sequence modeling.
+
+    Attributes:
+        f_array (FeatureArray): The feature array that stores symbols in the oracle.
+        latent (list): A list containing latent state information for compressed states.
     """
 
     def __init__(self, **kwargs):
+        """
+        Initializes the VMO object with parameters passed via **kwargs.
+        Resets the latent state information and initializes the feature array.
+        
+        Args:
+            **kwargs: Arbitrary keyword arguments for configuring the VMO instance.
+        """
         super(VMO, self).__init__(**kwargs)
         self.kind = 'a'
 
@@ -30,6 +44,12 @@ class VMO(FactorOracle):
         self.latent = []
 
     def reset(self, **kwargs):
+        """
+        Resets the VMO oracle by re-initializing its attributes and feature array.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for resetting the VMO instance.
+        """
         super(VMO, self).reset(**kwargs)
 
         self.kind = 'a'
@@ -41,7 +61,16 @@ class VMO(FactorOracle):
         self.latent = []
 
     def _dvec(self, new_symbol, k):
-        """docstring"""
+        """
+        Calculates the distance vector between a new symbol and previous states.
+
+        Args:
+            new_symbol (np.ndarray): The new symbol being added to the oracle.
+            k (int): The index of the state to compute the distance from.
+
+        Returns:
+            np.ndarray: The computed distance vector.
+        """
         if self.params['dfunc'] == 'other':
             return dist.cdist([new_symbol],
                               self.f_array[self.basic_attributes['trn'][k]],
@@ -61,7 +90,14 @@ class VMO(FactorOracle):
                           metric=self.params['dfunc'])[0]
 
     def _complete_method(self, i, pi_1, suffix_candidate):
-        """docstring"""
+        """
+        Handles state transitions and links when the complete method is used.
+
+        Args:
+            i (int): The current state index.
+            pi_1 (int): The previous state index.
+            suffix_candidate (list): A list of suffix candidates based on distance calculations.
+        """
         if not suffix_candidate:
             self.basic_attributes['sfx'][i] = 0
             self.basic_attributes['lrs'][i] = 0
@@ -79,7 +115,15 @@ class VMO(FactorOracle):
                 self.basic_attributes['data'][self.basic_attributes['sfx'][i]])
 
     def _non_complete_method(self, k, i, pi_1, suffix_candidate):
-        """docstring"""
+        """
+        Handles state transitions when the non-complete method is used.
+
+        Args:
+            k (int): The current suffix state.
+            i (int): The current state index.
+            pi_1 (int): The previous state index.
+            suffix_candidate (int): The suffix candidate index.
+        """
         if k is None:
             self.basic_attributes['sfx'][i] = 0
             self.basic_attributes['lrs'][i] = 0
@@ -95,55 +139,60 @@ class VMO(FactorOracle):
                 self.basic_attributes['data'][self.basic_attributes['sfx'][i]])
 
     def _temporary_adjustment(self, i):
+        """
+        Makes temporary adjustments to the latent states and suffixes during backtracking.
+
+        Args:
+            i (int): The current state index.
+        """
         k = self._find_better(
             i, self.basic_attributes['data'][i - self.basic_attributes['lrs'][i]])
         if k is not None:
             self.basic_attributes['lrs'][i] += 1
             self.basic_attributes['sfx'][i] = k
 
-        self.basic_attributes['rsfx'][self.basic_attributes['sfx'][i]].append(
-            i)
+        self.basic_attributes['rsfx'][self.basic_attributes['sfx'][i]].append(i)
 
         if self.basic_attributes['lrs'][i] > self.statistics['max_lrs'][i - 1]:
             self.statistics['max_lrs'].append(self.basic_attributes['lrs'][i])
         else:
-            self.statistics['max_lrs'].append(
-                self.statistics['max_lrs'][i - 1])
+            self.statistics['max_lrs'].append(self.statistics['max_lrs'][i - 1])
 
-        comp_1 = self.statistics['avg_lrs'][i - 1] * \
-            ((i - 1.0) / (self.statistics['n_states'] - 1.0))
-        comp_2 = self.basic_attributes['lrs'][i] * \
-            (1.0 / (self.statistics['n_states'] - 1.0))
+        comp_1 = self.statistics['avg_lrs'][i - 1] * ((i - 1.0) / (self.statistics['n_states'] - 1.0))
+        comp_2 = self.basic_attributes['lrs'][i] * (1.0 / (self.statistics['n_states'] - 1.0))
         self.statistics['avg_lrs'].append(comp_1 + comp_2)
 
     def add_state(self, new_symbol, method='inc', verbose=False):
+        """
+        Adds a new state to the VMO, updating state transitions, suffixes, and latent state information.
+
+        Args:
+            new_symbol (np.ndarray): The new symbol to be added.
+            method (str): The method used for state update ('inc' for incremental or 'complete').
+            verbose (bool): Whether to print timing information for state addition.
+        """
         start_time = time.time()
-        """Create new state and update related links and compressed state"""
         self.basic_attributes['sfx'].append(0)
         self.basic_attributes['rsfx'].append([])
         self.basic_attributes['trn'].append([])
         self.basic_attributes['lrs'].append(0)
 
-        # Experiment with pointer-based
         self.f_array.add(new_symbol)
 
         self.statistics['n_states'] += 1
         i = self.statistics['n_states'] - 1
 
-        # assign new transition from state i-1 to i
         self.basic_attributes['trn'][i - 1].append(i)
         k = self.basic_attributes['sfx'][i - 1]
         pi_1 = i - 1
 
-        # iteratively backtrack suffixes from state i-1
         suffix_candidate = (0, [])[method == 'complete']
 
         while k is not None:
             dvec = self._dvec(new_symbol, k)
             suffix = np.where(dvec < self.params['threshold'])[0]
 
-            if len(suffix) == 0:  # if no transition from suffix
-                # Add new forward link to unvisited state
+            if len(suffix) == 0:
                 self.basic_attributes['trn'][k].append(i)
                 pi_1 = k
                 if method != 'complete':
@@ -155,12 +204,10 @@ class VMO(FactorOracle):
                 suffix_candidate = self.basic_attributes['trn'][k][new_s]
                 break
             elif method == 'complete':
-                new_s = self.basic_attributes['trn'][k][suffix[np.argmin(
-                    dvec[suffix])]]
+                new_s = self.basic_attributes['trn'][k][suffix[np.argmin(dvec[suffix])]]
                 suffix_candidate.append((new_s, np.min(dvec)))
             else:
-                suffix_candidate = self.basic_attributes['trn'][k][suffix[np.argmin(
-                    dvec[suffix])]]
+                suffix_candidate = self.basic_attributes['trn'][k][suffix[np.argmin(dvec[suffix])]]
                 break
 
             if method == 'complete':
@@ -171,13 +218,8 @@ class VMO(FactorOracle):
         else:
             self._non_complete_method(k, i, pi_1, suffix_candidate)
 
-        # Temporary adjustment
         self._temporary_adjustment(i)
 
         if verbose:
-            print("I %i --- %s seconds ---" % (self.statistics['n_states'], time.time() - start_time))
-
-        #image = gen_plot.start_draw(self)
-        #name = r'data\oracles\oracle_at_' + str(i) + '.PNG'
-        #image.save(name)
-
+            print(f"I {self.statistics['n_states']} --- {time.time() - start_time} seconds ---")
+            
